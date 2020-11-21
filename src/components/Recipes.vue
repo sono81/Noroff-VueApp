995f0fa843a2ee439a505f7dc9bce334efd024a9
Sondre Nordjore <sonoskole@outlook.com>
2020-11-21 17:21:46 +0100

    setup recipe.vue

---
+++ src/components/Recipes.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="card">
+        <img />
+        <div class="card-info">
+            <span class="recipe"></span><br />
+            <span class="ingredients"></span><br />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Recipes',
+}
+</script>
+
+<style scoped>
+.card {
+        border: 3px solid #666666;
+        width: 360px;
+        padding: 20px;
+        text-align: center;
+      }
+
+      .card img {
+          margin-bottom: 20px;
+      }
+
+      .card-info {
+          background-color: #eeeeee;
+          padding: 10px;
+      }
+
+      .recipe {
+          font-size: 1.4em;
+      }
+
+      .recipe a {
+        text-decoration: none;
+        color: #1b8b2a;
+      }
+      
+      .ingredients {
+          color:#111111;
+      }
+</style>
