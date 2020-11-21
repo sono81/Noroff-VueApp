3acda2c0b265d970da6b831103b72e56db88872c
Sondre Nordjore <sonoskole@outlook.com>
2020-11-21 20:36:27 +0100

    setup recipe page

--- src/components/Recipes.vue
+++ src/components/Recipes.vue
@@ -1,46 +1,54 @@
 <template>
-    <div class="card">
-        <img />
-        <div class="card-info">
-            <span class="recipe"></span><br />
-            <span class="ingredients"></span><br />
-        </div>
+  <div class="card">
+    <img :src="thumbnail" />
+    <div class="card-info">
+      <h2 class="recipe">
+        <a :href="link" target="_blank">{{ title }}</a>
+      </h2>
+      <br />
+      <p class="ingredients">{{ ingredients }}</p>
+      <br />
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Recipes',
-}
+  name: "Recipes",
+  props: ["thumbnail", "link", "title", "ingredients"],
+};
 </script>
 
 <style scoped>
+
 .card {
-        border: 3px solid #666666;
-        width: 360px;
-        padding: 20px;
-        text-align: center;
-      }
+margin: 0 auto;
+  border: 3px solid #666666;
+  width: 360px;
+  padding: 20px;
+  text-align: center;
+  margin-bottom: 20px;
+}
 
-      .card img {
-          margin-bottom: 20px;
-      }
+.card img {
+  margin-bottom: 20px;
+}
 
-      .card-info {
-          background-color: #eeeeee;
-          padding: 10px;
-      }
+.card-info {
+  background-color: #eeeeee;
+  padding: 10px;
+}
 
-      .recipe {
-          font-size: 1.4em;
-      }
+.recipe {
+  font-size: 1.4em;
+}
 
-      .recipe a {
-        text-decoration: none;
-        color: #1b8b2a;
-      }
-      
-      .ingredients {
-          color:#111111;
-      }
+.recipe a {
+  text-decoration: none;
+  color: #1b8b2a;
+}
+
+.ingredients {
+  color: #111111;
+}
 </style>
