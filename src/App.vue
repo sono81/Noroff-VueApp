b1e36b2ade299ae4078df96d008555ab8bba212e
Sondre Nordjore <sonoskole@outlook.com>
2020-11-21 20:43:05 +0100

    registered Recipe page in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <recipe-page />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RecipePage from './RecipePage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    RecipePage
   }
 }
 </script>
