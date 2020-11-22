139254b8e4d5de323ac0e8c897bf42cb3ea103a5
Sondre Nordjore <sonoskole@outlook.com>
2020-11-22 14:36:10 +0100

    Add banner to template, import banner and register in components. Add margin top

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,35 @@
 <template>
   <div id="app">
+    <banner />
     <recipe-page />
   </div>
 </template>
 
 <script>
 import RecipePage from './RecipePage.vue'
+import Banner from './components/Banner.vue'
+
 
 export default {
   name: 'App',
   components: {
-    RecipePage
+    RecipePage,
+    Banner
   }
 }
 </script>
 
 <style>
+html, body {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 120px;
 }
 </style>
