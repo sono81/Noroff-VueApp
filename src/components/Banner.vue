0300eb5f8d52ef40e77b44eb84cc7e5198d4fa25
Sondre Nordjore <sonoskole@outlook.com>
2020-11-22 14:33:45 +0100

    add and setup banner.vue

---
+++ src/components/Banner.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="banner">
+        <h1>Recipes</h1>
+    </div>
+</template>
+
+<style scoped>
+.banner {
+    position: fixed;
+    background-color: rebeccapurple;
+    color: white;
+    top: 0;
+    width: 100%;
+    height: 80px;
+    box-shadow: 0px 5px 10px rgba(33, 33, 33, 0.75);
+}
+</style>
